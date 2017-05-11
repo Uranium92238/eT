cc91c38f4b7bd0c9ec8c5a89545ede55ad7fae77
Eirik Kjønstad <eirikkjonstad@dhcp-10-24-4-155.wlan.ntnu.no>
2017-05-11 15:41:31 +0200

    Added two more lines to calc proc class

--- src/calc_procedures_class.F90
+++ src/calc_procedures_class.F90
@@ -1,14 +1,17 @@
 module calc_procedures_class
 !
 !!
-!!                         Calculation procedures class module                                 
-!!              Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017         
+!!                       Calculation procedures class module                                 
+!!            Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017         
 !!
 !!    The procedures class is used for two purposes. Wavefunctions possess 
 !!    "implemented" and "tasks" objects, which are both instances of the class 
 !!    calculation procedures. If one of the tasks requested is not implemented
 !!    for the wavefunction in question, the program stops, letting the user know
 !!    that the requested functionality is not a part of eT (yet).
+!!
+!!    Tasks are determined during the read of the input (see the main program).
+!!    Implemented are set by the wavefunction's initialization routine.
 !! 
 !!
 !
