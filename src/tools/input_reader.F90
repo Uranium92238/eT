208df805cf37f93bee8aae7ad949a8c994363acc
Eirik Kjønstad <eirikkjonstad@dhcp-10-24-4-155.wlan.ntnu.no>
2017-05-11 14:43:43 +0200

    Fixed an issue in the input reader

--- src/tools/input_reader.F90
+++ src/tools/input_reader.F90
@@ -1,21 +1,25 @@
 module input_reader
 !
-!                          Input reader module                                 
-!        Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017 
+!!
+!!    Input reader module                                 
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017 
+!!
 !
    use types
    use input_output
    use calc_procedures_class
    use calc_settings_class
 !
+   implicit none
+!
 contains
 !
 !
    subroutine method_reader(unit_input, method)
-!
-!     Method Reader
-!     Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017
-!
+!!
+!!    Method Reader
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017
+!!
       implicit none 
 !
       integer(i15), intent(in) :: unit_input
@@ -65,17 +69,16 @@ contains
 !
 !
    subroutine calculation_reader(unit_input, tasks)
-!
-!     Calculation Reader
-!     Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017
-!
-!     Reads the calculation from the input file and initializes the 
-!     tasks requested of the wavefunction.
-!
+!!
+!!    Calculation Reader
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017
+!!
+!!    Reads the calculation from the input file and initializes the 
+!!    tasks requested of the wavefunction.
+!!
       implicit none 
 !
       type(calc_procedures) :: tasks 
-!
 !
       integer(i15), intent(in) :: unit_input
 !
@@ -83,78 +86,83 @@ contains
 !
       character(len=40) :: line 
 !
-      do ! General do loop - ends when it reaches 'exit'
+      read(unit_input,'(a40)') line   
 !
-         read(unit_input,'(a40)') line   
+      do while (line(1:1) == '!' .or. trim(line) == '') ! Comment or blank line: read the next line
 !
-         do while (line(1:1) == '!' .or. trim(line) == '') ! Comment or blank line: read the next line
+         read(unit_input,'(a40)') line 
 !
-            read(unit_input,'(a40)') line 
+      enddo
 !
-         enddo
+      if (trim(line) == 'Calculation:') then
 !
-         if (trim(line) == 'Calculation:') then
+         do ! Read calculations 
 !
-            do
+            read(unit_input,'(a40)') line
 !
-               read(unit_input,'(a40)') line
+            do while (line(1:1) == '!' .or. trim(line) == '') ! Comment or blank line: read the next line
+               read(unit_input,'(a40)') line 
+            enddo
 !
-               do while (line(1:1) == '!' .or. trim(line) == '') ! Comment or blank line: read the next line
-                  read(unit_input,'(a40)') line 
-               enddo
+            if (line(1:1) == '.') then 
 !
-               if (line(1:1) == '.') then 
+               calculation = trim(line(2:40))
 !
-                  calculation = trim(line(2:40))
+!              Test for which type, set the logical in tasks, and cycle!
 !
-!                 Test for which type, set the logical in tasks, and cycle!
+               if (calculation == 'ground_state') then
 !
-                  if (trim(line(2:40)) == 'ground_state') then
+                  tasks%do_ground_state = .true.
+                  cycle
 !
-                     tasks%do_ground_state = .true.
-                     exit
+               elseif (calculation == 'excited_state') then
 !
-                  else
+                  tasks%do_excited_state = .true.
+                  cycle 
 !
-                     write(unit_output,*) 'Input error: calculation ',trim(line(2:40)),' not recognized.'
-                     stop
+               elseif (calculation == 'properties') then
 !
-                  endif
+                  tasks%do_properties = .true. 
+                  cycle 
 !
                else
-! 
-                  write(unit_output,*) 'Input error: line ',trim(line),' not recognized.'
+!
+                  write(unit_output,*) 'Input error: calculation ',trim(line(2:40)),' not recognized.'
                   stop
 !
                endif
 !
-            enddo
+            elseif (trim(line) == 'Settings:') then 
 !
-         elseif (trim(line) == 'Settings:') then 
+               backspace(unit_input)
+               exit ! escape the do loop
 !
-            backspace(unit_input)
-            exit ! escape the do loop
+            else
+! 
+               write(unit_output,*) 'Input error: line ',trim(line),' not recognized.'
+               stop
 !
-         else
+            endif
 !
-            write(unit_output,*) 'Input error: expected calculation section, not the line ',trim(line),'.'
-            stop ! Terminate program
+         enddo
 !
-         endif
+      else
 !
-      enddo ! End general do loop
+         write(unit_output,*) 'Expected calculation settings, not ',trim(line),'.'
+!
+      endif
 !
    end subroutine calculation_reader
 !
 !
    subroutine settings_reader(unit_input, settings)
-!
-!     Settings Reader
-!     Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017
-!
-!     Reads the calculation settings from the input file and initializes the 
-!     settings requested of the wavefunction.
-!
+!!
+!!    Settings Reader
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, May 2017
+!!
+!!    Reads the calculation settings from the input file and initializes 
+!!    the settings requested of the wavefunction.
+!!
       implicit none 
 !
       type(calc_settings) :: settings 
