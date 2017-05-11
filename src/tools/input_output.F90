b34493aabd9641674a32704139f2535683540f6f
Eirik Kjønstad <eirikkjonstad@dhcp-10-24-4-155.wlan.ntnu.no>
2017-05-11 13:31:44 +0200

    Removed duplicate vec_print routine

--- src/tools/input_output.F90
+++ src/tools/input_output.F90
@@ -37,29 +37,4 @@ contains
    end subroutine generate_unit_identifier
 !
 !
-   subroutine vec_print(vec,dim_1,dim_2)
-!!
-!!    Vector print
-!!    Written by Eirik F. Kjønstad and Sarai D. Folkestad, March 2017
-!!
-!!    A vector is printed with a compound index (p q) of dimension (dim_1 x dim_2)
-!!
-      implicit none
-!
-      integer(i15) :: p = 0, q = 0, pq = 0
-!
-      integer(i15), intent(in) :: dim_1,dim_2
-      real(dp), dimension(dim_1, dim_2), intent(in) :: vec
-!
-      do q = 1, dim_2
-         do p = 1, dim_1
-!
-            write(unit_output,*) p, q, vec(p,q)
-!
-         enddo
-      enddo
-!
-   end subroutine vec_print
-!
-!
 end module input_output
