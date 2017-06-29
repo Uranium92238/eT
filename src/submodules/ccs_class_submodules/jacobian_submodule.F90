7aac51498db2e7a85331097c71a900fe43c372aa
Eirik Kjønstad <eirikkjonstad@dhcp-10-24-5-46.wlan.ntnu.no>
2017-06-29 11:02:39 +0200

    Some cleanup in documentation and indentation

--- src/submodules/ccs_class_submodules/jacobian_submodule.F90
+++ src/submodules/ccs_class_submodules/jacobian_submodule.F90
@@ -6,7 +6,10 @@ submodule (ccs_class) jacobian
 !!
 !!    Contains the following family of procedures of the CCS class:
 !!
-!!    Major cleanup needed here!!!!
+!!    jacobian_ccs_transformation: directs the transformation by A.
+!!    jacobian_ccs_a1:             adds the A1 term to the transformed vector. 
+!!    jacobian_ccs_b1:             adds the B1 term to the transformed vector.
+!!
 !
    implicit none 
 !
@@ -14,153 +17,159 @@ submodule (ccs_class) jacobian
 contains
 !
 !
-      module subroutine jacobian_ccs_transformation_ccs(wf, c_a_i)
+   module subroutine jacobian_ccs_transformation_ccs(wf, c_a_i)
 !!
-!!       Jacobian transformation
-!!       Written by Eirik F. Kjønstad and Sarai D. Folkestad, May 2017
+!!    Jacobian CCS transformation
+!!    Written by Eirik F. Kjønstad and Sarai D. Folkestad, May 2017
 !!
-         implicit none
-!
-         class(ccs) :: wf 
-         real(dp), dimension(wf%n_v, wf%n_o)   :: c_a_i       
-!
-         real(dp), dimension(:,:), allocatable :: rho_a_i
-!
-         call allocator(rho_a_i, wf%n_v, wf%n_o)
-         rho_a_i = zero
-!
-!        A1-term
-!
-         call wf%jacobian_ccs_a1(c_a_i,rho_a_i)
-!
-!        B1-term
-!
-         call wf%jacobian_ccs_b1(c_a_i,rho_a_i)
-!
-         call dcopy((wf%n_o)*(wf%n_v), rho_a_i, 1, c_a_i, 1)
-!
-         call deallocator(rho_a_i, wf%n_v, wf%n_o)
-!
-      end subroutine jacobian_ccs_transformation_ccs
-!
-      module subroutine jacobian_ccs_a1_ccs(wf,rho,c1)
+!!    Directs the transformation by the CCSD Jacobi matrix,
 !!
-!!       A1 contribution to right transform of Jacobian
-!!       Written by Eirik F. Kjønstad and Sarai D. Folkestad
+!!       A_mu,nu = < mu | exp(-T) [H, tau_nu] exp(T) | nu >. 
 !!
-!!       Calculates the A1 term of the right transform of the
-!!       Jacobian,
+!!    In particular,
 !!
-!!       A1: sum_b F_ab*c_bi - sum_j F_ji*c_aj
+!!       rho_mu = (A c)_mu = sum_ck A_mu,ck c_ck.
+!! 
+!!    On exit, c is overwritten by rho. 
 !!
-!!       and adds it to the rho vector.
-!!
-         implicit none
+      implicit none
 !
-         class(ccs) :: wf
-         real(dp), dimension(wf%n_v,wf%n_o) :: c1
-         real(dp), dimension(wf%n_v,wf%n_o) :: rho
+      class(ccs) :: wf 
+      real(dp), dimension(wf%n_v, wf%n_o)   :: c_a_i       
+!
+      real(dp), dimension(:,:), allocatable :: rho_a_i
+!
+      call allocator(rho_a_i, wf%n_v, wf%n_o)
+      rho_a_i = zero
+!
+!     Add the CCS terms of the transformation 
+!
+      call wf%jacobian_ccs_a1(c_a_i,rho_a_i)
+      call wf%jacobian_ccs_b1(c_a_i,rho_a_i)
+!
+      call dcopy((wf%n_o)*(wf%n_v), rho_a_i, 1, c_a_i, 1)
+!
+      call deallocator(rho_a_i, wf%n_v, wf%n_o)
+!
+   end subroutine jacobian_ccs_transformation_ccs
+!
+!
+   module subroutine jacobian_ccs_a1_ccs(wf,rho,c1)
+!!
+!!    Jacobian CCS A1
+!!    Written by Eirik F. Kjønstad and Sarai D. Folkestad
+!!
+!!    Calculates the A1 term,
+!!
+!!       sum_b F_ab*c_bi - sum_j F_ji*c_aj
+!!
+!!    and adds it to the rho vector.
+!!
+      implicit none
+!
+      class(ccs) :: wf
+!
+      real(dp), dimension(wf%n_v,wf%n_o) :: c1
+      real(dp), dimension(wf%n_v,wf%n_o) :: rho
 !   
+!     sum_b F_a_b * c_b_i
 !
-!        sum_b F_a_b * c_b_i
+      call dgemm('N', 'N',    &
+                  wf%n_v,     &
+                  wf%n_o,     &
+                  wf%n_v,     &
+                  one,        &
+                  wf%fock_ab, &
+                  wf%n_v,     &
+                  c1,         &
+                  wf%n_v,     &
+                  one,        &
+                  rho,        &
+                  wf%n_v)
 !
-         call dgemm('N', 'N',     &
-                     wf%n_v,      &
-                     wf%n_o,      &
-                     wf%n_v,      &
-                     one,         &
-                     wf%fock_ab,  &
-                     wf%n_v,      &
-                     c1,          &
-                     wf%n_v,      &
-                     one,         &
-                     rho,         &
-                     wf%n_v)
+!     - sum_j c_a_j * F_j_i
 !
-!        - sum_j c_a_j * F_j_i
+      call dgemm('N','N',     &
+                  wf%n_v,     &
+                  wf%n_o,     &
+                  wf%n_o,     &
+                  -one,       &
+                  c1,         &
+                  wf%n_v,     &
+                  wf%fock_ij, &
+                  wf%n_o,     &
+                  one,        &
+                  rho,        &
+                  wf%n_v)
 !
-         call dgemm('N','N',      &
-                     wf%n_v,      &
-                     wf%n_o,      &
-                     wf%n_o,      &
-                     -one,        &
-                     c1,          &
-                     wf%n_v,      &
-                     wf%fock_ij,  &
-                     wf%n_o,      &
-                     one,         &
-                     rho,         &
-                     wf%n_v)
-!
-      end subroutine jacobian_ccs_a1_ccs
+   end subroutine jacobian_ccs_a1_ccs
 !
 !
-      module subroutine jacobian_ccs_b1_ccs(wf,rho,c1)
+   module subroutine jacobian_ccs_b1_ccs(wf,rho,c1)
 !!
-!!       B1 contribution to right transform of Jacobian
-!!       Written by Eirik F. Kjønstad and Sarai D. Folkestad
+!!    Jacobian CCS B1 
+!!    Written by Eirik F. Kjønstad and Sarai D. Folkestad, May 2017
 !!
-!!       Calculates the B1 term of the right transform of the
-!!       Jacobian,
+!!    Calculates the B1 term,
 !!
-!!       B1: sum_bj L_aijb*c_bj = sum_bj (2*g_aijb-g_abji)c_bj
+!!       sum_bj L_aijb*c_bj = sum_bj (2*g_aijb-g_abji)c_bj,
 !!
-!!       and adds it to the rho vector.
+!!    and adds it to the rho vector.
 !!
-         implicit none
+      implicit none
 !
-         class(ccs) :: wf   
-         real(dp), dimension(wf%n_v,wf%n_o) :: c1
-         real(dp), dimension(wf%n_v,wf%n_o) :: rho       
+      class(ccs) :: wf
+!   
+      real(dp), dimension(wf%n_v,wf%n_o) :: c1
+      real(dp), dimension(wf%n_v,wf%n_o) :: rho       
 
 !
-!        Integrals
+!     Integrals
 !
-         real(dp), dimension(:,:), allocatable :: L_ai_J
-         real(dp), dimension(:,:), allocatable :: L_jb_J
-         real(dp), dimension(:,:), allocatable :: L_ji_J
-         real(dp), dimension(:,:), allocatable :: L_ab_J
+      real(dp), dimension(:,:), allocatable :: L_ai_J
+      real(dp), dimension(:,:), allocatable :: L_jb_J
+      real(dp), dimension(:,:), allocatable :: L_ji_J
+      real(dp), dimension(:,:), allocatable :: L_ab_J
 !
-         real(dp), dimension(:,:), allocatable :: g_ai_jb
-         real(dp), dimension(:,:), allocatable :: g_ab_ji
-         real(dp), dimension(:,:), allocatable :: L_ai_jb
+      real(dp), dimension(:,:), allocatable :: g_ai_jb
+      real(dp), dimension(:,:), allocatable :: g_ab_ji
+      real(dp), dimension(:,:), allocatable :: L_ai_jb
 !
-!        Reorderings of c and rho
+!     Reorderings of c and rho
 !
-         real(dp), dimension(:,:), allocatable :: c_jb
+      real(dp), dimension(:,:), allocatable :: c_jb
 !
-!        Batching variables
+!     Batching variables
 !
-         integer(i15) :: b_batch = 0, b_first = 0, b_last = 0, b_length = 0
-         integer(i15) :: required = 0, available = 0, n_batch = 0, batch_dimension = 0
-         integer(i15) :: max_batch_length = 0
+      integer(i15) :: b_batch = 0, b_first = 0, b_last = 0, b_length = 0
+      integer(i15) :: required = 0, available = 0, n_batch = 0, batch_dimension = 0
+      integer(i15) :: max_batch_length = 0
 !
-!        Indices
+!     Indices
 !
-         integer(i15) :: a = 0, b = 0
-         integer(i15) :: i = 0, j = 0
+      integer(i15) :: a = 0, b = 0
+      integer(i15) :: i = 0, j = 0
 !
-         integer(i15) :: ab = 0
-         integer(i15) :: ai = 0, jb = 0
-         integer(i15) :: ji = 0
+      integer(i15) :: ab = 0
+      integer(i15) :: ai = 0, jb = 0
+      integer(i15) :: ji = 0
 !
-         logical :: reorder
+      logical :: reorder
 !
+!     Preparing for batching over b
 !
-!        Preparing for batching over b
-!
-         required = max(((wf%n_o)**2)*((wf%n_v)**2)                              &  !
+      required = max(((wf%n_o)**2)*((wf%n_v)**2)                                 &  !
                         + 2*(wf%n_J)*(wf%n_v)**2 + 2*(wf%n_J)*(wf%n_v)*(wf%n_o), &  ! Needed to get L_ab_J
                         2*((wf%n_o)**2)*((wf%n_v)**2)                            &  !
                         + (wf%n_J)*(wf%n_v)**2 + (wf%n_J)*(wf%n_o)**2,           &  ! Needed to get g_ab_ij
                         3*((wf%n_o)**2)*((wf%n_v)**2))                              ! Needed to get L_ai_jb
 !
-         required = 4*required ! In words
-         available = get_available()
+      required = 4*required ! In words
+      available = get_available()
 !
-         batch_dimension  = wf%n_v ! Batch over the virtual index b
-         max_batch_length = 0      ! Initilization of unset variables 
-         n_batch          = 0
+      batch_dimension  = wf%n_v ! Batch over the virtual index b
+      max_batch_length = 0      ! Initilization of unset variables 
+      n_batch          = 0
 !
       call num_batch(required, available, max_batch_length, n_batch, batch_dimension)           
 !
@@ -311,8 +320,7 @@ contains
 !
       enddo ! Looping over batches
 !
+   end subroutine jacobian_ccs_b1_ccs
 !
-      end subroutine jacobian_ccs_b1_ccs
 !
-!
-end submodule
+end submodule jacobian
