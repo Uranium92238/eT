a68b37a7698d20ace99654a821495fb85994da7d
Sarai Dery Folkestad <sarai@dhcp-10-24-6-118.wlan.ntnu.no>
2017-08-15 18:11:02 +0200

    test for ifort

--- src/ccs_class.F90
+++ src/ccs_class.F90
@@ -700,10 +700,9 @@ module ccs_class
          real(dp), dimension(wf%tasks%n_singlet_states,1) :: eigenvalues_Re
          real(dp), dimension(wf%tasks%n_singlet_states,1) :: eigenvalues_Im
 !
-         real(dp), dimension(reduced_dim, wf%tasks%n_singlet_states) :: solution_vectors_reduced
-!
          integer(i15) :: reduced_dim
          integer(i15) :: n_new_trials
+         real(dp), dimension(reduced_dim, wf%tasks%n_singlet_states) :: solution_vectors_reduced
 !
       end subroutine construct_next_trial_vectors_ccs
 !
