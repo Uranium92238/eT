0f038d595402a8f6b0f09bf97119530ff6ab764e
Sarai Dery Folkestad <sarai@dhcp-10-24-4-240.wlan.ntnu.no>
2017-09-22 12:36:42 +0200

    CNTO thresholds in input

--- src/tools/input_reader.F90
+++ src/tools/input_reader.F90
@@ -331,6 +331,34 @@ contains
                   elseif (setting == 'cnto') then 
 !
                      mlcc_settings%cnto = .true.
+!
+!                    Get thresholds if in input
+!
+                     read(unit_input,'(a40)') line
+                     if(trim(line)=='cnto_threshold_o') then
+!
+                        read(unit_input,*) mlcc_settings%delta_o
+!
+                        read(unit_input,'(a40)') line
+!
+                        if(trim(line)=='cnto_threshold_v') then
+                          read(unit_input,*) mlcc_settings%delta_v
+                        endif
+! 
+                     elseif(trim(line)=='cnto_threshold_v') then
+!
+                        read(unit_input,*) mlcc_settings%delta_v
+!
+                        read(unit_input,'(a40)') line
+!
+                        if(trim(line)=='cnto_threshold_o') then
+                          read(unit_input,*) mlcc_settings%delta_o
+                        endif
+! 
+                     endif
+!
+!                    Done
+!
                      cycle
 !
                   elseif (setting == 'CCS') then
