a3f6c0bfb1a04d15d516405b45d64fb7169794bd
Sarai Dery Folkestad <sarai@dhcp-10-24-4-240.wlan.ntnu.no>
2017-10-19 10:44:07 +0200

    New definition of M

--- src/submodules/mlcc2_class_submodules/orbital_partitioning_submodule.F90
+++ src/submodules/mlcc2_class_submodules/orbital_partitioning_submodule.F90
@@ -1130,6 +1130,7 @@ contains
 !
       real(dp), dimension(:,:), allocatable :: solution
       real(dp), dimension(:,:), allocatable :: R_a_i
+      real(dp), dimension(:,:), allocatable :: R_a_i_sum
       real(dp), dimension(:,:), allocatable :: M_i_j
       real(dp), dimension(:,:), allocatable :: N_a_b
       real(dp), dimension(:,:), allocatable :: M
@@ -1149,7 +1150,7 @@ contains
       integer(i15) :: info
       integer(i15) :: i = 0, j = 0, a = 0, b = 0, ai = 0, ij = 0
 !
-      real(dp) :: trace, ddot, sum_o, sum_v
+      real(dp) :: trace, ddot, sum_o, sum_v, norm
 !
 !     ::::::::::::::::::::::::::::::::::::::::::::::
 !     -::- Construct CNTO transformation matrix -::-
@@ -1171,45 +1172,58 @@ contains
       call allocator(M_i_j, wf%n_o, wf%n_o)
       M_i_j = zero
 !
+      call allocator(R_a_i_sum, (wf%n_o)*(wf%n_v), 1)
+      R_a_i_sum = zero
+!
       call allocator(N_a_b, wf%n_v, wf%n_v)
       N_a_b = zero
 !
-!     Construct M and N
+!     Construct N
 !
       lower_level_n_singlet_states = CCS_factor_n_singlet_states*(wf%tasks%n_singlet_states)
 !
       do state = 1, lower_level_n_singlet_states
          read(unit=unit_solution, rec=state) (R_a_i(i , 1), i = 1, (wf%n_o)*(wf%n_v))
 !
-         call dgemm('T', 'N', &
-                     wf%n_o,  &
-                     wf%n_o,  &
-                     wf%n_v,  &
-                     one,     &
-                     R_a_i,   &
-                     wf%n_v,  &
-                     R_a_i,   &
-                     wf%n_v,  &
-                     one,     &
-                     M_i_j,   &
-                     wf%n_o)
+         if (state .le. wf%tasks%n_singlet_states) then
 !
-         call dgemm('N', 'T', &
-                     wf%n_v,  &
-                     wf%n_v,  &
-                     wf%n_o,  &
-                     one,     &
-                     R_a_i,   &
-                     wf%n_v,  &
-                     R_a_i,   &
-                     wf%n_v,  &
-                     one,     &
-                     N_a_b,   &
-                     wf%n_v)
+            call daxpy((wf%n_o)*(wf%n_v), one, R_a_i, 1, R_a_i_sum, 1) 
+!
+         endif 
+!
+            call dgemm('N', 'T', &
+                        wf%n_v,  &
+                        wf%n_v,  &
+                        wf%n_o,  &
+                        one,     &
+                        R_a_i,   &
+                        wf%n_v,  &
+                        R_a_i,   &
+                        wf%n_v,  &
+                        one,     &
+                        N_a_b,   &
+                        wf%n_v)
 !
       enddo
 !
+      norm = ddot((wf%n_o)*(wf%n_v), R_a_i, 1, R_a_i_sum, 1)
+      call dscal((wf%n_o)*(wf%n_v), one/norm, R_a_i_sum, 1)
+!
+      call dgemm('T', 'N',       &
+                     wf%n_o,     &
+                     wf%n_o,     &
+                     wf%n_v,     &
+                     one,        &
+                     R_a_i_sum,  &
+                     wf%n_v,     &
+                     R_a_i_sum,  &
+                     wf%n_v,     &
+                     one,        &
+                     M_i_j,      &
+                     wf%n_o)
+!
       call deallocator(R_a_i,(wf%n_o)*(wf%n_v), 1)
+      call deallocator(R_a_i_sum,(wf%n_o)*(wf%n_v), 1)
 !
 !     Done with file
 !
