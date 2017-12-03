ac2a0163825dfaeea07062b7936cb68ae3d4702e
Eirik Kjønstad <eirikkjonstad@dhcp-10-24-4-143.wlan.ntnu.no>
2017-12-03 15:24:34 +0100

    Mem manager now handles batching objects

--- src/batching_index_class.F90
+++ src/batching_index_class.F90
@@ -22,14 +22,28 @@ module batching_index_class
 !
    type :: batching_index 
 !
-      integer(i15) :: first       ! Current first value of index 
-      integer(i15) :: last        ! Current last value of index
-      integer(i15) :: length      ! Current length of batch (last - first + 1)
-      integer(i15) :: max_length  ! Maximum length of batch (most batches will be of this size, but typically not all)
+!     Values relating the limits and length of the current batch 
+!     (set by determine_limits procedure for a given batch)
 !
-      integer(i15) :: full_length ! Full length of index (e.g., typically n_vir for virtual index)
+      integer(i15) :: first      = 0 ! Current first value of index 
+      integer(i15) :: last       = 0 ! Current last value of index
+      integer(i15) :: length     = 0 ! Current length of batch (last - first + 1)
+!
+!     Values relating the the size of the batch and the total number of batches 
+!     (set by memory manager routines)
+!
+      integer(i15) :: max_length = 0  ! Maximum length of batch (most batches will be of this size, but typically not all)
+      integer(i15) :: num_batches = 0 ! The number of batches in total for the index 
+!
+!     Value that must be initialized by user 
+!
+      integer(i15) :: index_dimension = 0 ! Full length of index (e.g., typically n_vir for virtual index)
 !
    contains 
+!
+!     Initialization routine (sets the index dimension)
+!
+      procedure :: init => init_batching_index
 !
 !     Routine that sets the batch dependent variables,
 !     first, last and length, based on which batch it is
@@ -40,6 +54,25 @@ module batching_index_class
 !
 !
 contains 
+!
+!
+   subroutine init_batching_index(batch_p, dimension)
+!!
+!!    Init (batching index) 
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, Dec 2017
+!!
+!!    Note: every batching index has to be initialized - otherwise it
+!!          will not work as expected!
+!!
+      implicit none 
+!
+      class(batching_index) :: batch_p
+!
+      integer(i15), intent(in) :: dimension 
+!
+      batch_p%index_dimension = dimension 
+!
+   end subroutine init_batching_index
 !
 !
    subroutine determine_limits_batching_index(batch_p, batch_number)
@@ -56,7 +89,7 @@ contains
 !     Determine limits of batch, q = first, first + 1, ..., last
 !
       batch_p%first = 1 + (batch_number-1)*(batch_p%max_length)
-      batch_p%last  = min((batch_p%max_length)+(batch_number-1)*(batch_p%max_length), batch_p%full_length)
+      batch_p%last  = min((batch_p%max_length)+(batch_number-1)*(batch_p%max_length), batch_p%index_dimension)
 !
 !     Calculate the length of the batch 
 !
