928a84d4452ddd5a6ddb59e9a7c04b997a5cb633
Eirik Kjønstad <eirikkjonstad@MBP-16.local>
2018-06-26 20:37:16 +0200

    A few things

--- src/integrals/integral_manager_class.F90
+++ src/integrals/integral_manager_class.F90
@@ -40,14 +40,32 @@ module integral_manager_class
 !
    interface
 !
-      module subroutine cholesky_decompose_integral_manager(integral, molecule)
 !
+      module subroutine cholesky_decompose_integral_manager(integral, molecule)
+!!
+!!       Cholesky decompose
+!!       Written by Sarai D. Folkestad and Eirik F. Kjønstad, 2018
+!!
          implicit none
 !
          class(integral_manager) :: integral
          class(molecular_system) :: molecule
 !
       end subroutine cholesky_decompose_integral_manager
+!
+!
+      module subroutine construct_two_electron_diagonal_integral_manager(integral, D_xy)
+!!
+!!       Construct two electron diagonal
+!!       Written by Sarai D. Folkestad and Eirik F. Kjønstad, 2018
+!!
+         implicit none
+!
+         class(integral_manager) :: integral
+!
+         real(dp), dimension(:,:) :: D_xy
+!
+      end subroutine construct_two_electron_diagonal_integral_manager
 !
    end interface
 !
--- src/integrals/submodules/cholesky_submodule.F90
+++ src/integrals/submodules/cholesky_submodule.F90
@@ -1,6 +1,8 @@
 submodule (integral_manager_class) cholesky
 !
 !!
+!!    Cholesky submodule
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, 2018
 !!
 !
    implicit none
@@ -11,18 +13,33 @@ contains
 !
    module subroutine cholesky_decompose_integral_manager(integral, molecule)
 !!
-!!
-!!
+!!    Cholesky decompose
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, 2018
 !!
       implicit none
 !
       class(integral_manager) :: integral
       class(molecular_system) :: molecule
 !
-      write(output%unit,*) ' Hei hei! '
+      real(dp), dimension(:,:), allocatable :: D_xy
+!
+      call integral%construct_two_electron_diagonal(D_xy)
 !
    end subroutine cholesky_decompose_integral_manager
-
+!
+!
+   module subroutine construct_two_electron_diagonal_integral_manager(integral, D_xy)
+!!
+!!    Construct two electron diagonal
+!!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, 2018
+!!
+      implicit none
+!
+      class(integral_manager) :: integral
+!
+      real(dp), dimension(:,:) :: D_xy
+!
+   end subroutine construct_two_electron_diagonal_integral_manager
 !
 !
 end submodule cholesky
