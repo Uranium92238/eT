9ae79d114ea43407027e01772b60a97de6c6d604
Sarai Dery Folkestad <sarai@dhcp-10-24-5-148.wlan.ntnu.no>
2018-08-09 15:00:54 +0200

    new thresholds for cholesky decomposition of density

--- src/wavefunctions/mlhf_class.F90
+++ src/wavefunctions/mlhf_class.F90
@@ -196,13 +196,13 @@ contains
       call dscal(wf%n_ao**2, half, wf%ao_density, 1)
 !
       call cholesky_decomposition_limited_diagonal(wf%ao_density, cholesky_vectors_occ, wf%n_ao, &
-                                                     n_vectors_occ, 1.0d-9, n_active_aos, active_aos)
+                                                     n_vectors_occ, 1.0d-3, n_active_aos, active_aos)
 !
       call mem%alloc(cholesky_vectors_virt, wf%n_ao, n_active_aos)
       cholesky_vectors_virt = zero
 !
       call cholesky_decomposition_limited_diagonal(ao_density_v, cholesky_vectors_virt, wf%n_ao, &
-                                                     n_vectors_virt, 1.0d-9, n_active_aos, active_aos)
+                                                     n_vectors_virt, 1.0d-3, n_active_aos, active_aos)
 !
       write(output%unit, *)n_vectors_occ, n_vectors_virt
       flush(output%unit)
