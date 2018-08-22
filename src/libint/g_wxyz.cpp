2b790f0a96a36b5e89b4e32e742426ea97a216c5
Sarai Dery Folkestad <sarai@dhcp-10-24-5-148.wlan.ntnu.no>
2018-08-22 15:17:45 +0200

    symmetric sum now parallel

--- src/libint/g_wxyz.cpp
+++ src/libint/g_wxyz.cpp
@@ -26,31 +26,21 @@ void get_ao_g_wxyz(double *g, long *s1, long *s2, long *s3, long *s4){
 
 	int thread = omp_get_thread_num();
 
-	const long shell1 = *s1 - 1;
-	const long shell2 = *s2 - 1;
-	const long shell3 = *s3 - 1;
-	const long shell4 = *s4 - 1;
-
-	auto shell2bf = basis.shell2bf(); // maps shell index to basis function index
-                                   // shell2bf[0] = index of the first basis function in shell 0
-                                   // shell2bf[1] = index of the first basis function in shell 1
-                                   // ...
-
 	const auto& buf_vec = electronic_repulsion_engines[thread].results(); // will point to computed shell sets
 
-	auto bf1 = shell2bf[shell1];  // First basis function in shell 1
-	auto n1 = basis[shell1].size(); // Number of basis functions in shell 1
+	//auto bf1 = shell2bf[shell1];  // First basis function in shell 1
+	auto n1 = basis[*s1 - 1].size(); // Number of basis functions in shell 1
 
-	auto bf2 = shell2bf[shell2];  // First basis function in shell 2
-	auto n2 = basis[shell2].size(); // Number of basis functions in shell 2
+	//auto bf2 = shell2bf[shell2];  // First basis function in shell 2
+	auto n2 = basis[*s2 - 1].size(); // Number of basis functions in shell 2
 
-	auto bf3 = shell2bf[shell3];  // First basis function in shell 3
-	auto n3 = basis[shell3].size(); // Number of basis functions in shell 3
+	//auto bf3 = shell2bf[shell3];  // First basis function in shell 3
+	auto n3 = basis[*s3 - 1].size(); // Number of basis functions in shell 3
 
-	auto bf4 = shell2bf[shell4];  // First basis function in shell 4
-	auto n4 = basis[shell4].size(); // Number of basis functions in shell 4
+	//auto bf4 = shell2bf[shell4];  // First basis function in shell 4
+	auto n4 = basis[*s4 - 1].size(); // Number of basis functions in shell 4
 
-	electronic_repulsion_engines[thread].compute(basis[shell1], basis[shell2], basis[shell3], basis[shell4]);
+	electronic_repulsion_engines[thread].compute(basis[*s1 - 1], basis[*s2 - 1], basis[*s3 - 1], basis[*s4 - 1]);
 
 	auto ints_1234 = buf_vec[0]; // Location of computed integrals
 
