69e11f4836f4eb6c4840e0c21d93a46fb7e4447d
Sarai Dery Folkestad <sarai@dhcp-10-24-5-148.wlan.ntnu.no>
2018-08-28 17:33:13 +0200

    logicals for integral io, and function to determine if t1 things are on file

--- src/integrals/mo_integral_tool_class.F90
+++ src/integrals/mo_integral_tool_class.F90
@@ -14,10 +14,14 @@ module mo_integral_tool_class
 !
    type :: mo_integral_tool
 !
-!     No attributes yet
+      logical, private :: eri_file           = .false.
+      logical, private :: eri_t1_file        = .false. 
+      logical, private :: cholesky_file      = .true.
+      logical, private :: cholesky_t1_file   = .false.
 !
    contains
 !
+      procedure :: need_t1 => need_t1_mo_integral_tool
 !
    end type mo_integral_tool
 !
@@ -25,6 +29,21 @@ module mo_integral_tool_class
 contains
 !
 !
+   logical function need_t1_mo_integral_tool(integrals)
+!!
+!!    Need t1
+!!    Written by Eirik F. Kjønstad and Sarai D. Folkestad, 2018
+!!
+!!    Returns true if neither t1-transformed integrals nor t1-transformed cholesky
+!!    vectors are on file.
+!!
+      implicit none
+!
+      class(mo_integral_tool) :: integrals 
+!
+      need_t1 = ((.not. eri_t1_file) .and. (.not. cholesky_t1_file))
+!
+   end function need_t1_mo_integral_tool
 !
 !
 end module mo_integral_tool_class
