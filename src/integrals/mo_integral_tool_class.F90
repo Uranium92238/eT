8935a931f210b89b5f4065ea82db800da0515323
Sarai Dery Folkestad <sarai@dhcp-10-24-5-148.wlan.ntnu.no>
2018-08-29 13:11:19 +0200

    L_ai_J

--- src/integrals/mo_integral_tool_class.F90
+++ src/integrals/mo_integral_tool_class.F90
@@ -8,6 +8,7 @@ module mo_integral_tool_class
    use file_class
    use disk_manager_class
    use memory_manager_class
+   use reordering
 !
    implicit none
 !
@@ -160,7 +161,7 @@ contains
    end subroutine get_cholesky_ia_mo_integral_tool
 !
 !
-   subroutine get_cholesky_ai_mo_integral_tool(integrals, L_ai_J, first_a, last_a, first_i, last_i)
+   subroutine get_cholesky_ai_mo_integral_tool(integrals, L_ai_J, t1, first_a, last_a, first_i, last_i)
 !!
 !!    Get Cholesky ia 
 !!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, Aug 2018
@@ -172,10 +173,19 @@ contains
       integer(i15), optional, intent(in) :: first_i, last_i
       integer(i15), optional, intent(in) :: first_a, last_a
 !
-      real(dp), dimension(:, :) :: L_ia_J
+      real(dp), dimension(:, :) :: L_ai_J
+      real(dp), dimension(integrals%n_v, integrals%n_o) :: t1
+      real(dp), dimension(:, :), allocatable :: L_bj_J, L_ji_J, L_ba_J, X_j_iJ, X_i_jJ, X_i_aJ
 !
-      integer(i15) :: full_first_a, full_last_a 
-      integer(i15) :: full_first_i, full_last_i
+      integer(i15) :: full_first_a, full_last_a, length_a 
+      integer(i15) :: full_first_i, full_last_i, length_i
+!
+      integer(i15) :: i, J, a, ai, aJ, current_a_batch, required
+!
+      type(batching_index) :: batch_a
+!
+      length_i = full_last_i - full_first_i + 1
+      length_a = full_last_a - full_first_a + 1
 !
       call integrals%set_full_index(full_first_i, 'f', 'o', first_i)
       call integrals%set_full_index(full_first_a, 'f', 'v', first_a)
@@ -183,9 +193,118 @@ contains
       call integrals%set_full_index(full_last_i, 'l', 'o', last_i)
       call integrals%set_full_index(full_last_a, 'l', 'v', last_a)
 !
-      call integrals%read_cholesky(L_ia_J, full_first_i, full_last_i, full_first_a, full_last_a)
+      call integrals%read_cholesky(L_ai_J, full_first_a, full_last_a, full_first_i, full_last_i)
 !
-   end subroutine get_cholesky_ia_mo_integral_tool
+      call mem%alloc(L_bj_J, (integrals%n_v)*(integrals%n_o), 1)
+      call integrals%read_cholesky(L_bj_J, 1, (integrals%n_v), 1, (integrals%n_o))
+!
+      call mem%alloc(X_i_jJ, length_i, (integrals%n_o)*(integrals%n_cholesky))
+!
+      call dgemm('T', 'N',                                  &
+                  length_i,                                 &
+                  (integrals%n_o)*(integrals%n_cholesky),   &
+                  integrals%n_v,                            &
+                  one,                                      &
+                  t1(1, full_first_i),                      &   ! t_b_i
+                  integrals%n_v,                            &
+                  L_bj_J,                                   &   ! L_b_jJ
+                  integrals%n_v,                            &
+                  zero,                                     &
+                  X_i_jJ,                                   &
+                  length_i)
+!
+      call mem%dealloc(L_bj_J, (integrals%n_v)*(integrals%n_o), 1)
+!
+      call mem%alloc(X_j_iJ,(integrals%n_o), length_i*(integrals%n_cholesky))
+!
+      call sort_123_to_213(X_i_jJ, X_j_iJ, length_i, (integrals%n_o), (integrals%n_cholesky))
+!
+      call mem%dealloc(X_i_jJ, length_i, (integrals%n_o)*(integrals%n_cholesky))
+!
+      call dgemm('N', 'N',                                  &
+                  length_a,                                 &
+                  (length_i)*(integrals%n_cholesky),        &
+                  integrals%n_o,                            &
+                  -one,                                     &
+                  t1(full_first_a, 1),                      & ! t_a_j
+                  integrals%n_v,                            &
+                  X_j_iJ,                                   &
+                  integrals%n_o,                            &
+                  one,                                      &
+                  L_ai_J,                                   & ! L_a_iJ
+                  length_a)
+!
+      call mem%dealloc(X_j_iJ, (integrals%n_o), length_i*(integrals%n_cholesky))
+!
+      call mem%alloc(L_ji_J, (integrals%n_o)*length_i, (integrals%n_cholesky))
+      call integrals%read_cholesky(L_ji_J, 1, (integrals%n_o), full_first_i, full_last_i)
+!
+      call dgemm('N', 'N',                                  &
+                  length_a,                                 &
+                  (length_i)*(integrals%n_cholesky),        &
+                  integrals%n_o,                            &
+                  -one,                                     &
+                  t1(full_first_a, 1),                      & ! t_a_j
+                  integrals%n_v,                            &
+                  L_ji_J,                                   & ! L_j_iJ
+                  integrals%n_o,                            &
+                  one,                                      &
+                  L_ai_J,                                   & ! L_a_iJ
+                  length_a)     
+!
+      call mem%dealloc(L_ji_J, (integrals%n_o)*length_i, (integrals%n_cholesky))
+!
+      call batch_a%init(length_a)
+!
+      required = length_a*(integrals%n_v)*(integrals%n_cholesky) &
+               + length_i*(length_a)*(integrals%n_cholesky)
+!
+      call mem%num_batch(batch_a, required)
+!
+      do current_a_batch = 1, batch_a%num_batches
+!
+         call batch_a%determine_limits(current_a_batch) 
+!
+         call mem%alloc(L_ba_J, batch_a%length*(integrals%n_v), (integrals%n_cholesky)) 
+!
+         call integrals%read_cholesky(L_ba_J, 1, (integrals%n_v), &
+                        batch_a%first + (integrals%n_o), batch_a%last + (integrals%n_o))
+!
+         call mem%alloc(X_i_aJ, length_i, (batch_a%length)*(integrals%n_cholesky)) 
+!
+         call dgemm('T', 'N',                               &
+                  length_i,                                 &
+                  (batch_a%length)*(integrals%n_cholesky),  &
+                  integrals%n_v,                            &
+                  -one,                                     &
+                  t1,                                       & ! t_b_i
+                  integrals%n_v,                            &
+                  L_ba_J,                                   & ! L_b_aJ
+                  integrals%n_v,                            &
+                  one,                                      &
+                  X_i_aJ,                                   & 
+                  length_i)  
+!        
+         call mem%dealloc(L_ba_J, batch_a%length*(integrals%n_v), (integrals%n_cholesky))  
+!
+         do i = 1, length_i
+            do a = 1, batch_a%length
+               do J = 1, (integrals%n_cholesky)
+!
+                  ai = length_a*(i - 1) + a + batch_a%first - 1
+                  aJ = length_a*(J - 1) + a
+!
+                  L_ai_J(ai, J) = L_ai_J(ai, J) + X_i_aJ(i, aJ)
+!
+               enddo
+            enddo
+         enddo
+!
+         call mem%dealloc(X_i_aJ, length_i, (batch_a%length)*(integrals%n_cholesky)) 
+!
+       enddo
+!
+   end subroutine get_cholesky_ai_mo_integral_tool
 !
 !
    subroutine set_full_index_mo_integral_tool(integrals, ind, pos, orb_space, red_ind)
