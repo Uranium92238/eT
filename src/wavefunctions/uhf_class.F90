237324a9cd548c9a56a44b4787657d0edd41f9e0
Eirik Kjønstad <eirikkjonstad@dhcp-10-24-7-95.wlan.ntnu.no>
2018-09-04 14:11:21 +0200

    Spin AO Fock matrices - untested

--- src/wavefunctions/uhf_class.F90
+++ src/wavefunctions/uhf_class.F90
@@ -199,19 +199,22 @@ contains
    end subroutine form_ao_density_uhf
 !
 !
-   subroutine construct_ao_spin_fock_uhf(wf, D, D_sigma, sigma)
+   subroutine construct_ao_spin_fock_uhf(wf, D, D_sigma, sigma, &
+                     sp_eri_schwarz, sp_eri_schwarz_list, n_s, h_wx, coulomb, exchange, precision)
 !!
 !!    Construct AO spin Fock 
 !!    Written by Sarai D. Folkestad and Eirik F. Kjønstad, Sep 2018 
 !!
 !!    The routine computes the alpha or beta Fock matrix, depending 
-!!    on the value of the spin 'sigma' (='alpha' or 'beta').  
+!!    on the value of the spin 'sigma' (='alpha' or 'beta'):
 !!
-!!    Note that this routine has no screening as of today,
-!!    given that UHF is mainly useful in SAD (for us, today), 
-!!    where efficiency really isn't a big concern. It uses 
-!!    full permutational symmetry, like its more efficient 
-!!    variant in the restricted HF class.
+!!       F_αβ^alpha = h_αβ + sum_γδ g_αβγδ D_γδ + sum_γδ g_αδγβ D_γδ^alpha 
+!!       F_αβ^beta  = h_αβ + sum_γδ g_αβγδ D_γδ + sum_γδ g_αδγβ D_γδ^beta 
+!!
+!!    Here the superscript refers to the spin function, while the subscripts
+!!    are AO indices. In contrast to the restricted routine, this one does 
+!!    not calculate the energy - a separate call is required to get the 
+!!    unrestricted Hartree-Fock energy. 
 !!
       implicit none 
 !
@@ -222,17 +225,112 @@ contains
 !
       character(len=*), intent(in) :: sigma 
 !
+      integer(i15), intent(in) :: n_s
+!
+      real(dp), dimension(wf%n_ao, wf%n_ao), intent(in) :: h_wx
+!
+      real(dp), dimension(n_s*(n_s + 1)/2, 2), intent(in)     :: sp_eri_schwarz
+      integer(i15), dimension(n_s*(n_s + 1)/2, 3), intent(in) :: sp_eri_schwarz_list
+!
+      integer(i15) :: thread, n_threads, omp_get_max_threads
+!
+      real(dp), dimension(:,:), allocatable :: F, sp_density_schwarz
+!
+      real(dp), optional :: coulomb, exchange, precision      ! Non-standard thresholds, optionals
+      real(dp) :: coulomb_thr, exchange_thr, precision_thr    ! Actual thresholds 
+!
+      integer(i15) :: n_sig_sp
+!
+      real(dp) :: max_D_schwarz, max_eri_schwarz
+!
+      real(dp), dimension(:,:), allocatable :: scaled_D_sigma ! = -2 * D_sigma
+!
+!     Set thresholds to ignore Coulomb and exchange terms,
+!     as well as the desired Libint integral precision  
+!
+      coulomb_thr = 1.0D-11 
+      if (present(coulomb)) coulomb_thr = coulomb 
+!
+      exchange_thr = 1.0D-11
+      if (present(exchange)) exchange_thr = exchange 
+!
+      precision_thr = 1.0D-14
+      if (present(precision)) precision_thr = precision 
+!
+!     Compute number of significant ERI shell pairs (the Fock construction 
+!     only loops over these shell pairs) and the maximum element 
+!
+      call wf%get_n_sig_eri_sp(n_sig_sp, sp_eri_schwarz, 1.0d-20)
+      max_eri_schwarz = get_abs_max(sp_eri_schwarz, n_s*(n_s + 1)/2)
+!
+!     Construct the Coulomb two electron part of the Fock matrix, using the screening vectors 
+!     and parallellizing over available threads (each gets its own copy of the Fock matrix)
+!
+      call mem%alloc(sp_density_schwarz, n_s, n_s)
+      call wf%construct_sp_density_schwarz(sp_density_schwarz, D)
+      max_D_schwarz = get_abs_max(sp_density_schwarz, n_s**2)
+!
+      n_threads = omp_get_max_threads()
+!
+      call mem%alloc(F, wf%n_ao, wf%n_ao*n_threads) ! [F(thread 1) F(thread 2) ...]
+      F = zero 
+!
+      call wf%ao_fock_coulomb_construction_loop(F, D, n_threads, max_D_schwarz, max_eri_schwarz,         & 
+                                                sp_density_schwarz, sp_eri_schwarz, sp_eri_schwarz_list, &
+                                                n_s, n_sig_sp, coulomb_thr, precision_thr,               &
+                                                wf%system%shell_limits)
+!
+!     Construct the Coulomb two electron part of the Fock matrix, using the screening vectors 
+!     and parallellizing over available threads (each gets its own copy of the Fock matrix)
+!
+      call mem%alloc(scaled_D_sigma, wf%n_ao, wf%n_ao)
+      scaled_D_sigma = -two*D_sigma 
+!
+      call wf%construct_sp_density_schwarz(sp_density_schwarz, scaled_D_sigma)
+      max_D_schwarz = get_abs_max(sp_density_schwarz, n_s**2)      
+!
+      call wf%ao_fock_exchange_construction_loop(F, scaled_D_sigma, n_threads, max_D_schwarz, max_eri_schwarz, & 
+                                                   sp_density_schwarz, sp_eri_schwarz, sp_eri_schwarz_list,    &
+                                                   n_s, n_sig_sp, exchange_thr, precision_thr,                 &
+                                                   wf%system%shell_limits)      
+!
+      call mem%dealloc(sp_density_schwarz, n_s, n_s)
+      call mem%dealloc(scaled_D_sigma, wf%n_ao, wf%n_ao)
+!
+!     Add the accumulated Fock matrix F into the correct Fock matrix
+!     (i.e., either the alpha or beta Fock matrix )
+!
       if (trim(sigma) == 'alpha') then 
 !
-         
+         wf%ao_fock_a = zero
+         do thread = 1, n_threads
+!
+            call daxpy(wf%n_ao**2, one, F(1, (thread-1)*wf%n_ao + 1), 1, wf%ao_fock_a, 1)
+!
+         enddo
+!
+         call symmetric_sum(wf%ao_fock_a, wf%n_ao)
+         wf%ao_fock_a = wf%ao_fock_a*half
 !
       elseif (trim(sigma) == 'beta') then 
+!
+         wf%ao_fock_b = zero
+         do thread = 1, n_threads
+!
+            call daxpy(wf%n_ao**2, one, F(1, (thread-1)*wf%n_ao + 1), 1, wf%ao_fock_b, 1)
+!
+         enddo 
+!
+         call symmetric_sum(wf%ao_fock_b, wf%n_ao)
+         wf%ao_fock_b = wf%ao_fock_b*half
 !
       else 
 !
          call output%error_msg('Did not recognize spin variable in construct_ao_fock:' // trim(sigma))
 !
       endif 
+!
+      call mem%dealloc(F, wf%n_ao, wf%n_ao*n_threads) 
 !
    end subroutine construct_ao_spin_fock_uhf
 !
