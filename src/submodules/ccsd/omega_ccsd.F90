400f2fab047489fcd2158836a8c0de14b4069fe7
Sarai Dery Folkestad <sarai@Sarais-MacBook-Pro.local>
2018-09-28 12:25:32 +0200

    Shell submodule for omega

---
+++ src/submodules/ccsd/omega_ccsd.F90
@@ -0,0 +1,18 @@
+submodule (ccsd_class) omega_ccsd
+!
+!!
+!!    Omega submodule (ccsd)
+!!    Written by Eirik F. Kjønstad, Sarai D. Folkestad, 
+!!    Andreas Skeidsvoll and Alice Balbi, 2018
+!!
+!
+   implicit none
+!
+!
+!
+!
+contains
+!
+!
+!
+end submodule
