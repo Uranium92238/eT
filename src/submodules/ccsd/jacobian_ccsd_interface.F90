dcdad6e7bab78348d0eb9afcc7fb953fe27f47fb
Sarai Dery Folkestad <sarai@Sarais-MacBook-Pro.local>
2018-09-29 17:02:10 +0200

    jacobian interface

--- src/submodules/ccsd/jacobian_ccsd_interface.F90
+++ src/submodules/ccsd/jacobian_ccsd_interface.F90
@@ -111,3 +111,133 @@
       real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
 !
    end subroutine jacobian_ccsd_c2_ccsd
+!
+!
+  module subroutine jacobian_ccsd_d2_ccsd(wf, rho_ai_bj, c_a_i)
+!!
+!!    Jacobian CCSD D2
+!!    Written by Sarai D. Folkestad, Eirik F. Kjønstad
+!!    and Andreas Skeidsvoll, 2018
+!!
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension(wf%n_v, wf%n_o), intent(in)           :: c_a_i
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
+!
+   end subroutine jacobian_ccsd_d2_ccsd
+!
+!
+    module subroutine jacobian_ccsd_e2_ccsd(wf, rho_ai_bj, c_ai_ck)
+!!
+!!    Jacobian CCSD E2
+!!    Written by Eirik F. Kjønstad, Sarai D. Folkestad
+!!    and Andreas Skeidsvoll, 2018
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)), intent(in) :: c_ai_ck
+!
+   end subroutine jacobian_ccsd_e2_ccsd
+!
+!
+   module subroutine jacobian_ccsd_f2_ccsd(wf, rho_ai_bj, c_ai_bj)
+!!
+!!       Jacobian CCSD F2
+!!       Written by Eirik F. Kjønstad, Sarai D. Folkestad
+!!       and Andreas Skeidsvoll, 2018
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)), intent(in) :: c_ai_bj
+!
+   end subroutine jacobian_ccsd_f2_ccsd
+!
+!
+   module subroutine jacobian_ccsd_g2_ccsd(wf, rho_ai_bj, c_ai_bj)
+!!
+!!    Jacobian CCSD G2
+!!    Written by Eirik F. Kjønstad, Sarai D. Folkestad
+!!    and Andreas Skeidsvoll, 2018
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)), intent(in) :: c_ai_bj
+!
+   end subroutine jacobian_ccsd_g2_ccsd
+!
+!
+   module subroutine jacobian_ccsd_h2_ccsd(wf, rho_ai_bj, c_ai_bj)
+!!
+!!       Jacobian CCSD H2
+!!       Written by Eirik F. Kjønstad, Sarai D. Folkestad
+!!       and Andreas Skeidsvoll
+!!
+         implicit none
+!
+         class(ccsd) :: wf
+!
+         real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
+         real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)), intent(in) :: c_ai_bj
+!
+   end subroutine jacobian_ccsd_h2_ccsd
+!
+!
+   module subroutine jacobian_ccsd_i2_ccsd(wf, rho_ai_bj, c_ai_bj)
+!!
+!!    Jacobian CCSD I2
+!!    Written by Eirik F. Kjønstad and Sarai D. Folkestad
+!!    and Andreas Skeidsvoll, 2018
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)) :: rho_ai_bj
+      real(dp), dimension((wf%n_o)*(wf%n_v), (wf%n_o)*(wf%n_v)), intent(in) :: c_ai_bj
+!
+   end subroutine jacobian_ccsd_i2_ccsd
+!
+!
+   module subroutine jacobian_ccsd_j2_ccsd(wf, rho_ab_ij, c_ab_ij)
+!!
+!!     Jacobian CCSD J2
+!!     Written by Eirik F. Kjønstad, Sarai D. Folkestad
+!!     and Andreas Skeidsvoll, 2018
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension((wf%n_v)**2, (wf%n_o)**2) :: rho_ab_ij
+      real(dp), dimension((wf%n_v)**2, (wf%n_o)**2), intent(in) :: c_ab_ij
+!
+   end subroutine jacobian_ccsd_j2_ccsd
+!
+!
+   module subroutine jacobian_ccsd_k2_ccsd(wf, rho_ab_ij, c_ab_ij)
+!!
+!!    Jacobian CCSD K2
+!!    Written by Eirik F. Kjønstad, Sarai D. Folkestad
+!!    and Andreas Skeidsvoll, 2018
+!!
+      implicit none
+!
+      class(ccsd) :: wf
+!
+      real(dp), dimension((wf%n_v)**2, (wf%n_o)**2) :: rho_ab_ij
+      real(dp), dimension((wf%n_v)**2, (wf%n_o)**2), intent(in) :: c_ab_ij
+!
+   end subroutine jacobian_ccsd_k2_ccsd
+!
