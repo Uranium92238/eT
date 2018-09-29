e1096844756852ad050b02d71108bae4f5055ed7
Sarai Dery Folkestad <sarai@Sarais-MacBook-Pro.local>
2018-09-29 12:32:37 +0200

    interface + added files

---
+++ src/submodules/ccsd/jacobian_ccsd.F90
@@ -0,0 +1,17 @@
+submodule (ccsd_class) jacobian_ccsd
+!
+!!
+!!    Jacobian submodule (ccsd)
+!!    Written by Eirik F. Kjønstad, Sarai D. Folkestad, 
+!!    and Andreas Skeidsvoll, 2018
+!!
+!
+   implicit none
+!
+!
+contains
+!
+!
+!
+!
+end submodule jacobian_ccsd
