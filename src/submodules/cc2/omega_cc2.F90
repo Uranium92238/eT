d94d2c0335fee4c25337ba316a958ae00c448c42
Sarai Dery Folkestad <sarai@dhcp-10-24-5-95.wlan.ntnu.no>
2018-12-18 14:08:45 +0100

    C1 term done

--- src/submodules/cc2/omega_cc2.F90
+++ src/submodules/cc2/omega_cc2.F90
@@ -83,6 +83,59 @@ contains
 !
       real(dp), dimension(wf%n_amplitudes, 1), intent(inout) :: omega
 !
+      real(dp), dimension(:,:), allocatable :: g_aibj
+      real(dp), dimension(:,:), allocatable :: L_aijb
+!
+      integer(i15) :: i, j, a, b
+      integer(i15) :: ai, aj, bi, bj, jb
+!
+      call mem%alloc(g_aibj, wf%n_o*wf%n_v, wf%n_o*wf%n_v)
+!
+      call wf%get_vovo(g_aibj)
+!
+      call mem%alloc(L_aijb, wf%n_o*wf%n_v, wf%n_o*wf%n_v)
+!
+      do i = 1, wf%n_o 
+         do j = 1, wf%n_o
+            do a = 1, wf%n_v
+!
+               ai = wf%n_v*(i-1) + a
+               aj = wf%n_v*(j-1) + a
+!
+               do b = 1, wf%n_v
+
+!
+                  bi = wf%n_v*(i-1) + b
+                  bj = wf%n_v*(j-1) + b
+                  jb = wf%n_o*(b-1) + j
+!
+                  L_aijb(ai, jb) = -two*(g_aibj(ai, bj)/(wf%fock_ab(a,a) + wf%fock_ab(b,b) &
+                                                      - wf%fock_ij(i,i) - wf%fock_ij(j,j))) &
+                                    + (g_aibj(aj, bi)/(wf%fock_ab(a,a) + wf%fock_ab(b,b) &
+                                                      - wf%fock_ij(i,i) - wf%fock_ij(j,j))) 
+!
+               enddo
+            enddo
+         enddo
+      enddo
+!
+      call mem%dealloc(g_aibj, wf%n_o*wf%n_v, wf%n_o*wf%n_v)
+!
+      call dgemm('N', 'N', &
+                  wf%n_v*wf%n_o, &
+                  wf%n_v*wf%n_o, &
+                  wf%n_v*wf%n_o, &
+                  one,           &
+                  L_aijb,        &
+                  wf%n_v*wf%n_o, &
+                  wf%fock_ia,    &
+                  wf%n_v*wf%n_o, &
+                  one,           &
+                  omega,         &
+                  wf%n_v*wf%n_o)
+!
+      call mem%dealloc(L_aijb, wf%n_o*wf%n_v, wf%n_o*wf%n_v)
+!
    end subroutine omega_cc2_c1_cc2
 !
 end submodule
