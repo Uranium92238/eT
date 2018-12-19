86cc957e9ed391ee70a900e3aa1a3820ac931022
Sarai Dery Folkestad <sarai@dhcp-10-24-5-95.wlan.ntnu.no>
2018-12-19 17:10:07 +0100

    added batching to calculate_energy and it still works without batching

--- src/wavefunctions/cc2_class.F90
+++ src/wavefunctions/cc2_class.F90
@@ -55,37 +55,78 @@ contains
 !
       integer(i15) :: i, j, a, b, ai, bj, ia, jb, ib, ja
 !
-      call mem%alloc(g_aibj, wf%n_v*wf%n_o, wf%n_v*wf%n_o)
-      call mem%alloc(g_iajb, wf%n_v*wf%n_o, wf%n_v*wf%n_o)
+      integer(i15) :: rec0, rec1_i, rec1_j, rec2
 !
-      call wf%get_vovo(g_aibj)
-      call wf%get_ovov(g_iajb)
+      integer(i15) :: current_i_batch, current_j_batch
 !
-      wf%energy = wf%hf_energy
+      type(batching_index) :: batch_i, batch_j
 !
-      do i = 1, wf%n_o
-         do j = 1, wf%n_o
-            do a = 1, wf%n_v
-               do b = 1, wf%n_v
+      rec0 = 0
 !
-                  ai = index_two(a, i, wf%n_v)
-                  ia = index_two(i, a, wf%n_o)
-                  bj = index_two(b, j, wf%n_v)
-                  jb = index_two(j, b, wf%n_o)
-                  ib = index_two(i, b, wf%n_o)
-                  ja = index_two(j, a, wf%n_o)
+      rec1_i = (wf%n_o)*(wf%n_v)*(wf%integrals%n_J)
+      rec1_j = (wf%n_o)*(wf%n_v)*(wf%integrals%n_J)
 !
-                  wf%energy = wf%energy + (wf%t1(a,i)*wf%t1(b,j) &
-                                       - (g_aibj(ai, bj))/(wf%fock_diagonal(wf%n_o+a,1) &
-                                                         + wf%fock_diagonal(wf%n_o+b,1) &
-                                                         - wf%fock_diagonal(i,1) - wf%fock_diagonal(j,1)))&
-                                       *(two*g_iajb(ia,jb)-g_iajb(ib,ja))
+      rec2 =  2*(wf%n_o**2)*(wf%n_v**2)
 !
+      call batch_i%init(wf%n_o)
+      call batch_j%init(wf%n_o)
+!
+      call mem%batch_setup(batch_i, batch_j, rec0, rec1_i, rec1_j, rec2)
+!
+      do current_i_batch = 1, batch_i%num_batches
+!
+         call batch_i%determine_limits(current_i_batch)
+!
+         do current_j_batch = 1, batch_j%num_batches
+!
+            call batch_j%determine_limits(current_j_batch)
+!
+            call mem%alloc(g_aibj, wf%n_v*(batch_i%length), wf%n_v*(batch_j%length))
+            call mem%alloc(g_iajb, wf%n_v*(batch_i%length), wf%n_v*(batch_j%length))
+!
+            call wf%get_vovo(g_aibj, &
+                              1, wf%n_v, &
+                              batch_i%first, batch_i%last, &
+                              1, wf%n_v, &
+                              batch_j%first, batch_j%last)
+!
+            call wf%get_ovov(g_iajb, &
+                              batch_i%first, batch_i%last, &
+                              1, wf%n_v, &
+                              batch_j%first, batch_j%last, &
+                              1, wf%n_v)
+!
+            wf%energy = wf%hf_energy
+!
+            do i = 1, batch_i%length
+               do j = 1, batch_j%length
+                  do a = 1, wf%n_v
+                     do b = 1, wf%n_v
+!
+                        ai = index_two(a, i, wf%n_v)
+                        ia = index_two(i, a, batch_i%length)
+                        bj = index_two(b, j, wf%n_v)
+                        jb = index_two(j, b, batch_j%length)
+                        ib = index_two(i, b, batch_i%length)
+                        ja = index_two(j, a, batch_j%length)
+!
+                        wf%energy = wf%energy + (wf%t1(a,i)*wf%t1(b,j) &
+                                             - (g_aibj(ai, bj))/(wf%fock_diagonal(wf%n_o + a, 1) &
+                                                               + wf%fock_diagonal(wf%n_o + b, 1) &
+                                                               - wf%fock_diagonal(i + batch_i%first - 1,1) &
+                                                               - wf%fock_diagonal(j + batch_j%first - 1,1)))&
+                                             *(two*g_iajb(ia,jb)-g_iajb(ib,ja))
+!
+                     enddo
+                  enddo
                enddo
             enddo
+!
+            call mem%dealloc(g_aibj, wf%n_v*(batch_i%length), wf%n_v*(batch_j%length))
+            call mem%dealloc(g_iajb, wf%n_v*(batch_i%length), wf%n_v*(batch_j%length))
+!
          enddo
       enddo
-!
 !
    end subroutine calculate_energy_cc2
 !
