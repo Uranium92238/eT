8a5e7a33e3a528da2e8f015d2764873eef576d5f
Sarai Dery Folkestad <sarai@dhcp-10-24-5-95.wlan.ntnu.no>
2018-12-20 13:03:22 +0100

    added eps_o and eps_v vectors in omega foutines for cc2

--- src/submodules/cc2/omega_cc2_interface.F90
+++ src/submodules/cc2/omega_cc2_interface.F90
@@ -14,7 +14,7 @@
    end subroutine construct_omega_cc2
 !
 !
-   module subroutine omega_cc2_a1_cc2(wf, omega)
+   module subroutine omega_cc2_a1_cc2(wf, omega, eps_o, eps_v)
 !!
 !!    Omega CC2 A1 term
 !!    Written by Eirik F. Kjønstad, Sarai D. Folkestad, 
@@ -25,11 +25,13 @@
       class(cc2), intent(in) :: wf
 !
       real(dp), dimension(wf%n_amplitudes, 1), intent(inout) :: omega
+      real(dp), dimension(wf%n_o), intent(in) :: eps_o
+      real(dp), dimension(wf%n_v), intent(in) :: eps_v
 !
    end subroutine omega_cc2_a1_cc2
 !
 !
-   module subroutine omega_cc2_b1_cc2(wf, omega)
+   module subroutine omega_cc2_b1_cc2(wf, omega, eps_o, eps_v)
 !!
 !!    Omega CC2 B1 term
 !!    Written by Eirik F. Kjønstad, Sarai D. Folkestad, 
@@ -40,11 +42,13 @@
       class(cc2), intent(in) :: wf
 !
       real(dp), dimension(wf%n_amplitudes, 1), intent(inout) :: omega
+      real(dp), dimension(wf%n_o), intent(in) :: eps_o
+      real(dp), dimension(wf%n_v), intent(in) :: eps_v
 !
    end subroutine omega_cc2_b1_cc2
 !
 !
-   module subroutine omega_cc2_c1_cc2(wf, omega)
+   module subroutine omega_cc2_c1_cc2(wf, omega, eps_o, eps_v)
 !!
 !!    Omega CC2 C1 term
 !!    Written by Eirik F. Kjønstad, Sarai D. Folkestad, 
@@ -55,5 +59,7 @@
       class(cc2), intent(in) :: wf
 !
       real(dp), dimension(wf%n_amplitudes, 1), intent(inout) :: omega
+      real(dp), dimension(wf%n_o), intent(in) :: eps_o
+      real(dp), dimension(wf%n_v), intent(in) :: eps_v
 !
    end subroutine omega_cc2_c1_cc2
