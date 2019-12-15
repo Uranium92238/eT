4c8f4e504abdd34fad78e31769856d5c2a8e472a
Sarai Dery Folkestad <Sarai@Sarais-MBP-2.lan>
2019-12-15 13:38:32 +0100

    Fixed. Was doing same shell pairs over and over when batching

--- src/solvers/cholesky/eri_cd_class.F90
+++ src/solvers/cholesky/eri_cd_class.F90
@@ -4013,6 +4013,8 @@ contains
 !
                   if (current_size_AB .lt. size_AB) then
 !
+                     construct_sp(sp) = .false. ! This shell pair should not be used again.
+!
                      if (A .ne. B) then 
 !
                         do x = 1, A_interval%length
