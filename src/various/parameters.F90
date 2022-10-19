ca93b1f0c8060951500c934f73023fe2166905e7
Sarai Dery Folkestad <sarai.d.folkestad@ntnu.no>
2022-10-19 10:30:50 -0700

    regenerate tests and name of minor

--- src/various/parameters.F90
+++ src/various/parameters.F90
@@ -37,7 +37,7 @@ module parameters
    integer, parameter :: minor_version = 9
    integer, parameter :: patch_version = 0
 !
-   character(len=27), parameter :: version_name = "J (development)"
+   character(len=27), parameter :: version_name = "Jazzy"
 !
 !  Integers
 !
